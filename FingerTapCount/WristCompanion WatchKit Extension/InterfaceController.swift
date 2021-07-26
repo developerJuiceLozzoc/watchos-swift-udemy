b769b34a29d2466d42b54b84f6399fafe918ee3c
lozzoc <reach.maddaloc@gmail.com>
2021-07-26 14:28:13 -0400

    added top cars app

--- FingerTapCount/WristCompanion WatchKit Extension/InterfaceController.swift
+++ FingerTapCount/WristCompanion WatchKit Extension/InterfaceController.swift
@@ -8,32 +8,41 @@
 import WatchKit
 import Foundation
 
-struct MarketRates: Codable {
-    var ask: Double
-    var bid: Double
-    var last: Double
-}
-
-struct BitResponse: Codable {
-    var display_name: String
-    var rates: MarketRates
-    var volume_btc: Double
+struct SpeedCarData {
+    var imageName: String
+    var carTitle: String
+    var topSpeed: String
 }
 
 
 class InterfaceController: WKInterfaceController {
     
-    @IBOutlet weak var price: WKInterfaceLabel!
-
+    @IBOutlet var carTableView: WKInterfaceTable!
+    
     override func awake(withContext context: Any?) {
+        guard let path = Bundle.main.path(forResource: "cars", ofType: "plist"),
+              let dict: NSDictionary = NSDictionary(contentsOfFile: path)
+        else { return }
         
+        carTableView.setNumberOfRows(dict.allKeys.count, withRowType: "CarTableRow")
+        
+        var indexPath: Int = 0
+        for (_,value) in dict {
+            if let carEntry = value as? Array<String>, carEntry.count == 2, let row = carTableView.rowController(at: indexPath) as? CarTableRow {
+                let carImage = carEntry[0]
+                let carSpeed = carEntry[1]
+                let carName = String(carImage.split(separator: "-")[0])
+                
+                row.datasource = SpeedCarData(imageName: carImage, carTitle: carName, topSpeed: carSpeed)
+                indexPath += 1
+                
+                
+            }
+        }
     }
     
     override func willActivate() {
-        price.setText("Loading...")
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.price.setText("$\(Int.random(in: 29_000...35_000))")
-        }
+       
     }
     
     override func didDeactivate() {
